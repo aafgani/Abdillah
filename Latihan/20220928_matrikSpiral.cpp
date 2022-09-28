54a29c60c3267492d766c2fd25876f6470baaf92
aafgani <andrya.afgani@gmail.com>
2022-09-28 10:36:40 +0700

    matrik spiral

---
+++ Latihan/20220928_matrikSpiral.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <string>
+
+void matrik_spiral()
+{
+    int m, n;
+    printf("Masukkan m n : ");
+    scanf("%d %d", &m, &n);
+
+    printf("m=%d, n=%d\n", m, n);
+
+    int counter = 1;
+    for (int i = 0; i < m; i++)
+    {
+        int counter2 = counter - 1;
+        for (int j = n; j > 0; j--)
+        {
+            if (i % 2 == 0)
+            {
+                printf("%d ", counter);
+            }
+            else
+            {
+                printf("%d ", counter2 + j);
+            }
+            counter++;
+        }
+        printf("\n");
+    }
+}
