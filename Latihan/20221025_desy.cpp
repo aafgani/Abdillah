2abd97218d7b732f3be129b93f66a8b5889372cd
aafgani <andrya.afgani@gmail.com>
2022-10-25 13:50:33 +0700

    desy

---
+++ Latihan/20221025_desy.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void run()
+{
+    int m, n;
+    printf("Masukkan m : ");
+    scanf("%d", &m);
+
+    printf("m=%d\n", m);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (i % 2 == 0)
+            {
+                printf("@ ");
+            }
+            else
+            {
+                printf("$ ");
+            }
+        }
+        printf("\n");
+    }
+}
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,10 @@
 #include "../Latihan/Latihan4.cpp"
 #include "../Latihan/Latihan5.cpp"
+#include "../Latihan/20220928_matrikSpiral.cpp"
+#include "../Latihan/20221025_desy.cpp"
 
 int main(int argc, char const *argv[])
 {
-    Latihan5();
+    run();
     return 0;
 }
